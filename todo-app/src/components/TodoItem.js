3c476fb3f3ed95e79b85f089921902761d4912e6
phubao1999 <phutuongbao1999@gmail.com>
2019-07-01 15:03:18 +0700

    new course

--- todo-app/src/components/TodoItem.js
+++ todo-app/src/components/TodoItem.js
@@ -1,52 +1,60 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 class TodoItem extends Component {
+  getStyle = () => {
+    return {
+      background: "#f4f4f4",
+      padding: "10px",
+      borderBottom: "1px #ccc dotted",
+      display: "flex",
+      justifyContent: "space-between",
+      textDecoration: this.props.todo.completed ? "line-through" : "none"
+    };
+  };
 
-    getStyle = () => {
-        return {
-            background: '#f4f4f4',
-            padding: '10px',
-            borderBottom: '1px #ccc dotted',
-            display: 'flex',
-            justifyContent: 'space-between',
-            textDecoration: this.props.todo.completed ? 'line-through' : 'none'
-        }
-    }
+  markComplete = e => {
+    console.log(this.props);
+  };
 
-    markComplete = (e) => {
-        console.log(this.props)
-    }
-
-    render() {
-        const {title, id} = this.props.todo
-        return (
-            <div style={this.getStyle()}>
-                <input 
-                    type="checkbox" 
-                    onChange={this.props.markComplete.bind(this,id)} 
-                /> 
-               <h3 style={{marginLeft: '5px'}}>{title}</h3> 
-               <button onClick={this.props.delTodo.bind(this, id)} style={btnStyle}>X</button>
-            </div>
-        );
-    }
+  render() {
+    const { title, id } = this.props.todo;
+    return (
+      <div style={this.getStyle()}>
+        <input
+          type="checkbox"
+          onChange={this.props.markComplete.bind(this, id)}
+        />{" "}
+        <h3
+          style={{
+            marginLeft: "5px"
+          }}
+        >
+          {" "}
+          {title}{" "}
+        </h3>{" "}
+        <button onClick={this.props.delTodo.bind(this, id)} style={btnStyle}>
+          X{" "}
+        </button>{" "}
+      </div>
+    );
+  }
 }
 
 TodoItem.propTypes = {
-    todos: PropTypes.object.isRequired,
-    markComplete: PropTypes.func.isRequired,
-    delTodo: PropTypes.func.isRequired
-}
+  todos: PropTypes.object.isRequired,
+  markComplete: PropTypes.func.isRequired,
+  delTodo: PropTypes.func.isRequired
+};
 
 const btnStyle = {
-    background: '#ff0000',
-    color: '#fff',
-    border: 'none',
-    padding: '5px 10px',
-    borderRadius: '50%',
-    cursor: 'pointer',
-    float: 'right'
-}
+  background: "#ff0000",
+  color: "#fff",
+  border: "none",
+  padding: "5px 10px",
+  borderRadius: "50%",
+  cursor: "pointer",
+  float: "right"
+};
 
 export default TodoItem;
--- todo-app/src/components/Todos.js
+++ todo-app/src/components/Todos.js
@@ -1,20 +1,24 @@
-import React, { Component } from 'react';
-import TodoItem from './TodoItem';
-import PropTypes from 'prop-types'
+import React, { Component } from "react";
+import TodoItem from "./TodoItem";
+import PropTypes from "prop-types";
 
 class Todos extends Component {
-   
-    render() {
-        return this.props.todos.map((todo) => (
-            <TodoItem key={todo.id} todo={todo} markComplete={this.props.markComplete} delTodo={this.props.delTodo} />
-        ))
-    }
+  render() {
+    return this.props.todos.map(todo => (
+      <TodoItem
+        key={todo.id}
+        todo={todo}
+        markComplete={this.props.markComplete}
+        delTodo={this.props.delTodo}
+      />
+    ));
+  }
 }
 
 Todos.propTypes = {
-    todos: PropTypes.object.isRequired,
-    markComplete: PropTypes.func.isRequired,
-    delTodo: PropTypes.func.isRequired
-}
+  todos: PropTypes.object.isRequired,
+  markComplete: PropTypes.func.isRequired,
+  delTodo: PropTypes.func.isRequired
+};
 
 export default Todos;
