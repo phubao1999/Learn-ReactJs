b6f339f06f5bf2cff4f894cd172a3a83e4165ea6
phubao1999 <phutuongbao1999@gmail.com>
2019-07-03 17:03:35 +0700

    More Page

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
+import {Switch, Route} from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
+import Details from './components/Details';
+import Default from './components/Default';
+import Cart from './components/Cart';
+import ProductList from './components/ProductList';
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-        Hello From App
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={ProductList}></Route>
+          <Route path="/details" component={Details}></Route>
+          <Route path="/cart" component={Cart}></Route>
+          <Route component={Default}></Route>
+        </Switch>
       </React.Fragment>
     );
   }
---
+++ react-app/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import {link} from 'react-router-dom';
+
+class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Hello From Navbar</h3>
+            </div>
+        );
+    }
+}
+
+export default Navbar;
