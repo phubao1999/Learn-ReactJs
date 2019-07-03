dd8cfd5c7434c0534be25ecd9cb3b795b79e5aa6
phubao1999 <phutuongbao1999@gmail.com>
2019-07-03 22:36:47 +0700

    Something i don't Really Know

--- react-app/src/components/Navbar.js
+++ react-app/src/components/Navbar.js
@@ -1,20 +1,20 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import logo from '../logo.svg';
-// import styled from 'styled-components';
+import styled from 'styled-components';
 import {ButtonContainer} from './Button';
 
 class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+      <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
         <Link to="/">
             <img src={logo} alt="store" className="navbar-brand" />
         </Link>
         <ul className="navbar-nav align-items-center">
             <li className="nav-item ml-5">
                 <Link to="/" className="nav-link">
-                    Product
+                    product
                 </Link>
             </li>
         </ul>
@@ -26,9 +26,18 @@ class Navbar extends Component {
                 my cart
             </ButtonContainer>
         </Link>
-      </nav>
+      </NavWrapper>
     );
   }
 }
+
+const NavWrapper = styled.nav`
+    background: var(--mainBlue);
+    .nav-link{
+        color:var(--mainWhite)!important;
+        font-size: 1.3rem;
+        text-transform: capitalize;
+    }
+`
 
 export default Navbar;
