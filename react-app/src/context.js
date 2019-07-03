77dd7dced1b64e906f1d3f387ea89153c42d36c4
phubao1999 <phutuongbao1999@gmail.com>
2019-07-04 02:55:06 +0700

    Almost Finish Product

--- react-app/src/context.js
+++ react-app/src/context.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
+import {storeProducts, detailProduct} from './data';
 
 const ProductContext = React.createContext();
 
 class ProductProvider extends Component {
-    
+    state={
+        products:storeProducts,
+        detailProduct:detailProduct
+    }
+    handleDetail = () => {
+        console.log('Hello From Detail')
+    }
+    addToCart = () => {
+        console.log('Hello From Cart')
+    }
     render() {
         return (
-            <ProductContext.Provider value="hello From Context">
+            <ProductContext.Provider value={{
+                ...this.state,
+                handleDetail: this.handleDetail,
+                addToCart: this.addToCart
+            }}>
                 {this.props.children}
             </ProductContext.Provider>
         );
